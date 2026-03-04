817aecb5538cbe15ab1596dfb7bd306fde1a781c
CarolinaTresch <tresccar@students.zhaw.ch>
2026-03-04 08:07:33 +0100

    LN2 Infos

--- views/home.py
+++ views/home.py
@@ -4,6 +4,11 @@ st.title("Meine erste Streamlit App")
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
+# LN2: Home_Site sollte folgende Infos beinhalten:
+# - Titel & kurze Beschreibung
+# - AUtoren
+# - wichtig: alle Studienden Mails der Mitwirkenden
+
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Aya Ramadan (ramadaya@students.zhaw.ch)
@@ -14,3 +19,4 @@ Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (
 
 Autor: Samuel Wehrli (wehs@zhaw.ch)
 """
+
